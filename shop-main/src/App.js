83bd9b671bacdc5fcf170f014487c11c1b9316be
nowaveosu <nowaveosu@gmail.com>
2023-02-23 21:58:20 +0900

    state done

--- shop-main/src/App.js
+++ shop-main/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post =  '강남 우동 맛집';
+  let post =  '역삼 우동 맛집';
+  let titleArr = ['남자코트 추천','강남 우동맛집','파이썬독학']
+  let [title,setTitle] = useState(titleArr);
+  let [logo, setLogo] = useState('ReactBlog');
 
-  return (
+return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red',fontSize:'16px'}}>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+        {title.map(el => 
+          <div className='list'>
+            <h4>{el}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        )}
     </div>
   );
 }
