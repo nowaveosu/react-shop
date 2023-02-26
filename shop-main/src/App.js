082b448638d18a566e14a5e51b4a8da8e2ca46a3
nowaveosu <nowaveosu@gmail.com>
2023-02-26 17:01:43 +0900

    2.26

--- shop-main/src/App.js
+++ shop-main/src/App.js
@@ -1,25 +1,31 @@
+/* eslint-disable */ 
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-
   let post =  '역삼 우동 맛집';
-  let titleArr = ['남자코트 추천','강남 우동맛집','파이썬독학']
-  let [title,setTitle] = useState(titleArr);
-  let [logo, setLogo] = useState('ReactBlog');
+  let [글제목,b] = useState(['독감','강남 우동맛집','파이썬독학']);
+  let [따봉,따봉변경] = useState(0);
 
 return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-        {title.map(el => 
-          <div className='list'>
-            <h4>{el}</h4>
-            <p>2월 17일 발행</p>
-          </div>
-        )}
+      <button>글수정</button>
+      <div className='list'>
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
